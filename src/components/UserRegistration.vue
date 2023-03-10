700d40163860a9d4cb4777e193abedbc8ac1cb22
markrusselbaral <nullcracker04@gmail.com>
2023-03-10 22:20:39 +0800

    added_quizzui

--- src/components/UserRegistration.vue
+++ src/components/UserRegistration.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container" v-if="!isDivHidden">
         <form id="form" @submit.prevent="saveUser">
             <h1>Registration</h1>
             <div class="input-control">
@@ -15,6 +15,26 @@
             <button type="submit" :disabled="isSubmitting" :class="{ 'btn-disabled': isSubmitting, 'btn-clicked': isClicked }">Proceed</button>
         </form>
     </div>
+
+
+
+    <div class="all-items-center" v-if="isDivHidden">
+         <center>
+            <div style="margin-top: 20px;">
+               <h1 class="header-title">Guess The Logo</h1>
+               <hr style="border: 3px solid #ff9100; border-radius: 10px; width: 100px; margin-bottom: 20px;" />
+               <div class="alert-box"></div>
+               <div class="hinl-place"></div>
+               <p class="qustion"></p>
+               <input type="text" class="answer" placeholder="Type Your Answer" />
+               <br/>
+               <button class="skip"> skip </button>
+               <button class="reveal"> reveal </button>
+               <br>
+               <input type="button" class="submit-btn" value="submit"/>
+            </div>
+         </center>
+      </div>
 </template>
 
 
@@ -29,6 +49,8 @@ export default {
       lastname: "",
       isSubmitting: false,
       isClicked: false,
+      isDivHidden: false,
+      
     };
   },
   methods: {
@@ -44,6 +66,8 @@ export default {
           }
         );
         console.log(response.data);
+        this.isDivHidden = true;
+        this.isDivHidden2 = false;
       } catch (error) {
         console.error(error);
       }finally {
@@ -56,6 +80,104 @@ export default {
 
 
 <style scoped>
+ .header-title
+         {
+         font-weight: 1000;
+         background: -webkit-linear-gradient(70deg,#33ccff 0%, #ff99cc 100%);
+         -webkit-background-clip: text;
+         -webkit-text-fill-color: transparent;
+         }
+         .answer
+         {
+         text-align: center;
+         min-width:280px;
+         padding: 10px;
+         font-size: 15px;
+         font-weight: bold;
+         border: 1px solid #000;
+         outline: none;
+         border-radius: 20px;
+         margin-bottom: 20px;
+         transition: 0.3s;
+         }
+         .answer:focus
+         {
+         border: 1px solid #ff9100;
+         box-shadow: 0 10px 20px #ff880079;
+         }
+         .submit-btn,.reveal,.skip
+         {
+         min-width:300px;
+         padding: 10px;
+         font-size: 15px;
+         border: 1px solid #ff9100;
+         outline: none;
+         border-radius: 20px;
+         background: #ff9100;
+         color: white;
+         text-transform: uppercase;
+         font-weight: bold;
+         cursor: pointer;
+         transition: 0.5s;
+         }
+         .submit-btn:hover
+         {
+         border: 1px solid #ff9100;
+         background: white;
+         color: #ff9100;
+         }
+         .reveal,.skip
+         {
+            min-width:147px;
+            margin-bottom: 10px;
+         }
+         .skip{background: red; border: 1px solid red;}.skip:hover{background: white; color: red; border: 1px solid red;}
+         .reveal{background: black; border: 1px solid black;}.reveal:hover{background: white; color: black; border: 1px solid black;}
+         .all-items-center
+         {
+         position: absolute;
+         top: 50%;left: 50%;
+         transform: translate(-50%, -50%);
+         box-shadow: 0 10px 25px rgba(92,99,105,.2);
+         padding: 20px;
+         border-radius: 10px;
+         background: white;
+         }
+         .qustion{
+         max-width: 300px;
+         font-weight: bold;
+         font-family: 'Poppins', sans-serif;
+         }
+         .hinl-place
+         {
+         width: 50%;
+         min-width: 153px;
+         font-size: 100px;
+         border: 1px solid #000;
+         outline: none;
+         border-radius: 20px;
+         }
+         .alert-box
+         {
+         max-width: 300px;
+         border: 1px solid #000;
+         outline: none;
+         border-radius: 20px;
+         padding: 6px;
+         font-weight: bold;
+         margin-bottom: 20px;
+         color: white;
+         display: none;
+         }
+
+
+
+
+
+
+
+
+
 body {
     background: linear-gradient(to right, #0f2027, #203a43, #2c5364);
     font-family: 'Poppins', sans-serif;
